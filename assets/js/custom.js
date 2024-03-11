15315fc5b792c549637db0429e051eb2d173a709
Yusuf Ozkan <77548712+yusufozkn@users.noreply.github.com>
2024-03-12 01:29:06 +0300

    Sticky Header
    header denemesi. sayfalarin genel gorunumu ayarlamasi ve renkler

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var header = document.querySelector('.navbar');
-  
+    var header = document.getElementById('mainNav');
+
     window.addEventListener('scroll', function () {
-      if (window.scrollY > 50) { // Sayfa aşağı kaydırıldığında
-        header.classList.add('sticky-header');
-      } else { // Sayfa yukarı çıkıldığında
-        header.classList.remove('sticky-header');
-      }
+        if (window.scrollY > 50) { // Sayfa aşağı kaydırıldığında
+            header.classList.add('sticky-header');
+        } else { // Sayfa yukarı çıkıldığında
+            header.classList.remove('sticky-header');
+        }
     });
-  });
+});
